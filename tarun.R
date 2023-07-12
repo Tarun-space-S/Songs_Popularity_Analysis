f7c140b64d0291f0db1f7c8852da045be8a91f40
Tarun S <legend432002@gmail.com>
2023-07-12 12:46:06 +0530

    data set included

--- tarun.R
+++ tarun.R
@@ -1,2 +1,8 @@
 getwd()
 setwd("D:/CODE/SUMMER/R_data_Analytics/data")
+data<-read.csv("Songs_dataset.csv")
+summary(data)
+str(data)
+library(ggplot2)
+ggplot(data,aes(x=Loudness,y=Liveness))+geom_point()
+ggplot(data,aes(x=Loudness,y=Views))+geom_point()

9629c4d464e41263d157f061ad3a6a1376aa1238
abhishekgit4737 <130521854+abhishekgit4737@users.noreply.github.com>
2023-07-12 23:31:54 +0530

    Update Abhishek.R

--- Abhishek.R
+++ Abhishek.R
@@ -2,6 +2,7 @@
 getwd()
 setwd("c:\\Users\\abhis\\OneDrive\\Documents\\summerCourse\\project\\")
 songstats<-read.csv("Songs_dataset.csv")
+View(songstats)
 
 nrow(songstats)
 ncol(songstats)
@@ -11,7 +12,7 @@ tail(songstats[, c("Artist","Track","Views")],n=10)
 str(songstats)#structure
 summary(songstats)
 songstats$Album
-levels(songstats$Track)
+
 
 songstats[4:30, c("Artist","Track","Views")]
 songstats[c(2,7), c("Artist","Track","Views")]
@@ -25,12 +26,45 @@ songstats$Energy_Loudness_Ratio<-NULL
 head(songstats[, c("Track","Energy","Loudness","Energy_Loudness_Ratio")],n=10)
 head(songstats[, c("Track","Energy","Loudness")],n=10)
 
+
 #filtering
-#display all song tracks that crossed 1 billion views
-filter1<-songstats$Views>1000000000
-songstats[filter1,c("Track","Views")]
+library(dplyr)
 
+#display all song tracks that crossed 2 billion views
+filter1 <- songstats %>%
+  filter(Views > 2000000000) %>%
+  select(Track, Views, Likes,Energy)
+filter1
 
-#display dataframe where income group is Low income
-filter2<-songstats$Artist=="Coldplay"
-songstats[filter2,"Track"]
+#display Coldplay Track
+filter2 <- filter(songstats, Artist == "Coldplay")
+filter2[,c("Track","Album","Energy")]
+
+#display in descending order of Views the Track details of Drake's Her Loss album
+filter3 <- filter(songstats, Artist == "Drake", Album == "Her Loss") %>%
+  arrange(desc(Views))
+filter3[,c("Artist","Album","Track","Views")]
+
+filter4 <- filter(songstats, Artist %in% c("Eminem", "Shakira") , Danceability > 0.7)%>%
+  arrange(desc(Danceability))
+filter4[,c("Artist","Track","Danceability")]
+
+#quick plot
+?qplot
+library("ggplot2")
+qplot(data=filter3,x=Track, y=Views,size=I(3)) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+qplot(data = filter1, x = Track, y = Views, size = I(3),colour="Views",shape=I(2),main="1 BILLION+ VIEWS" ,geom = "boxplot") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+qplot(data = filter4, x = Track, y = Danceability, colour = Likes) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+                                                                    
+#ggplot
+ggplot(data = filter2, aes(x = Track, y = Energy, colour = Energy, group = 1)) +
+  geom_point() +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+#histogram
+s<-ggplot(data=filter1,aes(x=Views))
+s+geom_histogram(binwidth = 10)
+s+geom_histogram(binwidth = 10,aes(fill="Views"))
+

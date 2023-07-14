628f54c6b3ec9ba5ceb28edf2f163a7d6105f6c7
Ujjwal Tiku <92386685+ujjwal-tiku@users.noreply.github.com>
2023-07-14 10:30:02 +0530

    Update regression-ujjwal.R

--- regression-ujjwal.R
+++ regression-ujjwal.R
@@ -1,35 +1,20 @@
 #commit
 getwd()
-#setwd("C:/Users/ujjwa/OneDrive/Desktop/r_files")
-setwd("D:/CODE/SUMMER/R_data_Analytics/data")
+setwd("C:/Users/ujjwa/OneDrive/Desktop/r_files")
 songs<-read.csv('spotify_songs.csv')
 View(songs)
 
 
-#regress 5 variables energy,loudness,valence,playlist genre and subgenre on views or track popularity
+#LM of 4 variables energy,loudness,valence,playlist genre and subgenre on views or track popularity
 popularity_fit_energy <- lm(formula = track_popularity ~ energy,data =songs)
-plot(songs$energy,songs$track_popularity)
-library(ggplot2)
-ggplot(songs,aes(x=track_popularity,y=energy))+geom_point()
-summary(popularity_fit_energy)
+summary(popularity_fit_energy)#hypothesis for each
 
 popularity_fit_loudness<-lm(formula = track_popularity ~loudness, data = songs)
-#plot(songs$loudness,songs$track_popularity)
-#abline(popularity_fit_loudness)
-summary(popularity_fit_loudness)
-library(ggplot2)
-ggplot(songs,aes(x=track_popularity,y=loudness))+geom_point()
+summary(popularity_fit_loudness)#hypo
 
-popularity_fit_valence <- lm(formula = track_popularity ~ valence,data =songs)
-#plot(songs$valence,songs$track_popularity)
-library(ggplot2)
-ggplot(songs,aes(x=track_popularity,y=valence))+geom_point()
-summary(popularity_fit_valence)
-
-#using playlist genre
-genre_fit <- lm(track_popularity ~ playlist_genre, data = songs)
-summary(genre_fit)
+popularity_fit_genre <- lm(formula = track_popularity ~ playlist_genre,data =songs)
 ggplot(songs,aes(x=track_popularity,y=playlist_genre))+geom_boxplot()
+summary(popularity_fit_genre)
 
 #using playlist genre#highest influence
 subgenre_fit <- lm(track_popularity ~ playlist_subgenre, data = songs)
@@ -49,8 +34,9 @@ songs %>%
 # track_popularity 	49	   60	    67	     77
 # energy	          0.59   0.71	  0.73	   0.84
 # loudness	       -6.6	  -5.8	 -5.3	    -4.2
-# valence	          0.39	 0.55	  0.55     0.72
 
 
 
-
+#Residuals
+res<-resid(subgenre_fit)
+qqnorm(res)

45e735cf774e2c03a015e9717de06941d57ccaeb
Tarun S <89335295+Tarun-space-S@users.noreply.github.com>
2023-09-20 22:27:35 +0530

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -1,51 +1,51 @@
 ?read.csv
 getwd()# Get the working directory
 setwd("D:/CODE/SUMMER/R_data_Analytics/data")# Replace file path with the file path in your system
-songstats<-read.csv("Songs_dataset.csv")
-View(songstats)
+data<-read.csv("Songs_dataset.csv")
+View(data)
 
-nrow(songstats)
-ncol(songstats)
-dim(songstats)
-head(songstats[, c("Artist","Track","Views")],n=10)
-tail(songstats[, c("Artist","Track","Views")],n=10)
-str(songstats)#structure
-summary(songstats)
-songstats$Album
+nrow(data)
+ncol(data)
+dim(data)
+head(data[, c("Artist","Track","Views")],n=10)
+tail(data[, c("Artist","Track","Views")],n=10)
+str(data)#structure
+summary(data)
+data$Album
 
 
-songstats[4:30, c("Artist","Track","Views")]
-songstats[c(2,7), c("Artist","Track","Views")]
-#check whether the first row of a songstats is itself a data frame
-is.data.frame(songstats[1,])
+data[4:30, c("Artist","Track","Views")]
+data[c(2,7), c("Artist","Track","Views")]
+#check whether the first row of a data is itself a data frame
+is.data.frame(data[1,])
 
-songstats$Energy_Loudness_Ratio <- songstats$Energy / songstats$Loudness
-head(songstats[, c("Track","Energy","Loudness","Energy_Loudness_Ratio")],n=10)
+data$Energy_Loudness_Ratio <- data$Energy / data$Loudness
+head(data[, c("Track","Energy","Loudness","Energy_Loudness_Ratio")],n=10)
 
-songstats$Energy_Loudness_Ratio<-NULL
-head(songstats[, c("Track","Energy","Loudness","Energy_Loudness_Ratio")],n=10)
-head(songstats[, c("Track","Energy","Loudness")],n=10)
+data$Energy_Loudness_Ratio<-NULL
+head(data[, c("Track","Energy","Loudness","Energy_Loudness_Ratio")],n=10)
+head(data[, c("Track","Energy","Loudness")],n=10)
 
 
 #filtering
 library(dplyr)
 
 #display all song tracks that crossed 2 billion views
-filter1 <- songstats %>%
+filter1 <- data %>%
   filter(Views > 2000000000) %>%
   select(Track, Views, Likes,Energy)
 filter1
 
 #display Coldplay Track
-filter2 <- filter(songstats, Artist == "Coldplay")
+filter2 <- filter(data, Artist == "Coldplay")
 filter2[,c("Track","Album","Energy")]
 
 #display in descending order of Views the Track details of Drake's Her Loss album
-filter3 <- filter(songstats, Artist == "Drake", Album == "Her Loss") %>%
+filter3 <- filter(data, Artist == "Drake", Album == "Her Loss") %>%
   arrange(desc(Views))
 filter3[,c("Artist","Album","Track","Views")]
 
-filter4 <- filter(songstats, Artist %in% c("Eminem", "Shakira") , Danceability > 0.7)%>%
+filter4 <- filter(data, Artist %in% c("Eminem", "Shakira") , Danceability > 0.7)%>%
   arrange(desc(Danceability))
 filter4[,c("Artist","Track","Danceability")]
 
